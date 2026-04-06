c2dedafaae48560b860b290f8acdb0f5cffc5bef
.raw <151in1hit@gmail.com>
2026-04-07 01:35:58 +0200

    Downgrade version from 5.0.0 to 4.2.0 😮‍💨

--- violent-ripper.user.js
+++ violent-ripper.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Violent Ripper
 // @namespace    https://github.com/crmbz0r/violent-ripper
-// @version      5.0.0
+// @version      4.2.0
 // @description  Rips websites using correct local paths and preserves the original folder structure
 // @match        *://*/*
 // @grant        GM_xmlhttpRequest
